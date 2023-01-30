fb0dc087104c51a1e7a194897c14aff1148f8e2a
CristianoNunes <cristianonunes99@gmail.com>
2023-01-29 22:05:57 -0300

    Add Routes

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
 import theme from "@theme/index";
 
 import { Loading } from "@components/Loading";
-import { Groups } from "@screens/Groups";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
