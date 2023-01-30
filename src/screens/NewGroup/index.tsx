797ea8d3f62ad6f445390d0683d049f7016ba3c2
CristianoNunes <cristianonunes99@gmail.com>
2023-01-29 22:53:47 -0300

    Finalized routes

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,22 +1,24 @@
-import { Container, Content, Icon } from "./styles";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
+import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup() {
+  const [group, setGroup] = useState("");
+
   const navigation = useNavigation();
 
-  const handleNew = () => {
-    navigation.navigate("players", { group: "Rocket" });
-  };
+  function handleNew() {
+    navigation.navigate("players", { group });
+  }
 
   return (
     <Container>
       <Header showBackButton />
-
       <Content>
         <Icon />
 
@@ -25,7 +27,7 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input placeholder="Nome da Turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
 
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
